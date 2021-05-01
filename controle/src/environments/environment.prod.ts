45460ebeb84006610236424b101c8d0e2ab27d02
GustavoNascimentoGarcia <gu1993@outlook.com.br>
2021-04-30 22:52:22 -0300

    conexão do firebase com o código

--- controle/src/environments/environment.prod.ts
+++ controle/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCxZ6F8wxYjyEvwWPtUtGpK2yms9p3qtIY',
+    authDomain: 'controle-2021.firebaseapp.com',
+    projectId: 'controle-2021',
+    storageBucket: 'controle-2021.appspot.com',
+    messagingSenderId: '440727557804',
+    appId: '1:440727557804:web:0d63f7311af8a6198c3109',
+    measurementId: 'G-SC7GTTJ9BK'
+  }
 };
--- controle/src/environments/environment.ts
+++ controle/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCxZ6F8wxYjyEvwWPtUtGpK2yms9p3qtIY',
+    authDomain: 'controle-2021.firebaseapp.com',
+    projectId: 'controle-2021',
+    storageBucket: 'controle-2021.appspot.com',
+    messagingSenderId: '440727557804',
+    appId: '1:440727557804:web:0d63f7311af8a6198c3109',
+    measurementId: 'G-SC7GTTJ9BK'
+  }
+
 };
 
 /*
