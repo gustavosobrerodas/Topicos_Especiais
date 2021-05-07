b8f03e8f0456acf685a8aef4c247c70a602edee2
GustavoNascimentoGarcia <gu1993@outlook.com.br>
2021-05-07 19:17:47 -0300

    configuração do firebase

--- controle/src/app/app.module.ts
+++ controle/src/app/app.module.ts
@@ -6,11 +6,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  ],
+
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
